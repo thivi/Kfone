c0c5471cc0175b92f2509cbb74896defd48ffed4
Krishnamohan Theviyanthan <theviyanthan@outlook.com>
2023-03-29 11:19:12 +0530

    Add customer

---
+++ Kfone/ViewModels/Add_CustomerViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Kfone.Core.Models;
+using Kfone.Core.Services;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+namespace Kfone.ViewModels
+{
+    public class Add_CustomerViewModel : ObservableObject
+    {
+        public Add_CustomerViewModel()
+        {
+        }
+
+        public void AddCustomer(Customer customer)
+        {
+            CustomerService.SetCustomer(customer);
+        }
+    }
+}
---
+++ Kfone/Views/Add_CustomerPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Kfone.Core.Models;
+using Kfone.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+
+namespace Kfone.Views
+{
+    public sealed partial class Add_CustomerPage : Page
+    {
+        public Add_CustomerViewModel ViewModel { get; } = new Add_CustomerViewModel();
+
+        public Add_CustomerPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            Tiers tier;
+            if(GoldTier.IsChecked == true) {
+                tier = Tiers.Gold;
+            } else if(SilverTier.IsChecked == true)
+            {
+                tier = Tiers.Silver;
+            } else if(PlatinumTier.IsChecked==true)
+            {
+                tier = Tiers.Platinum;
+            } else
+            {
+                tier = Tiers.None;
+            }
+            Customer customer = new Customer { name = Name.Text, address = address.Text, contactNo = contactNo.Text, dob = DateTime.Parse(dob.Date.ToString()), profilePic = profilePic.Text, tier = tier };
+            ViewModel.AddCustomer(customer);
+            this.Frame.Navigate(typeof(CustomersPage));
+        }
+    }
+}
